4539ec569b9a5c39ce59e3d380bb6e7da89913a7
oyewunmi oluwaseyi <42620838+oyewunmio@users.noreply.github.com>
2021-07-24 19:19:32 +0100

    Changes made in school
    Database was dropped and re created

--- schoool/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [
-    path('', HomePageView.as_view(),name='home'),
-    path('login/',loginView, name='login'),
-    path('logout/',user_logout,name='logout'),
-]
--- schoool/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import ParentSignUpView
-from django.views.generic import CreateView
-
-urlpatterns = [
-    path('createParent/',ParentSignUpView, name='parent_register'),
-    #path('createStudent/', StudentForm,name = 'student_register'),
-    #path(route, view, name=str)
-]
